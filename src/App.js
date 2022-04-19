28f922cc2119c9815ed3a49df51b1c4e52f0d591
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-19 07:21:03 +0200

    add clock component and test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ClockForSure from './clockComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          <ClockForSure/>
+        </h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/clockComponent.js
@@ -0,0 +1,11 @@
+import React,{Component}from "react";
+
+
+class ClockForSure extends Component{
+    render(){
+        return 'test clock';
+    }
+
+}
+
+export default ClockForSure;
